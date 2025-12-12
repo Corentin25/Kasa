afe58322a306a2f66e305266adc69f7a13ecf316
Corentin Busi <corentinbusi@gmail.com>
2025-12-12 11:35:48 +0100

    Initialisation projet + ajout différents dossiers/fichiers

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import logo from "./assets/logo.png";
+
+export function App() {
+  return (
+    /* Élément provisoire */
+    <div>
+      <img src={logo} alt="Logo de Kasa" />
+    </div>
+  );
+}
