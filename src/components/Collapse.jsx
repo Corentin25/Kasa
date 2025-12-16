0e0349b8586e1ec0b64d4251d1b274dfbe4ebef5
Corentin Busi <corentinbusi@gmail.com>
2025-12-16 18:00:08 +0100

    Mise en place page À Propos + Collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,7 +1,27 @@
-export function Collapse() {
-  return (
-    <div>
+import { useState } from "react";
+import arrow from "../assets/arrow_back.png";
+import "../styles/collapse.scss";
 
+export function Collapse({ title, description }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openDownClsp = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="collapse">
+      <div className="buttonClps" onClick={openDownClsp}>
+        <h3>{title}</h3>
+        <img
+          src={arrow}
+          alt="Flèche ouvrir / fermer"
+          className={`arrowClps ${isOpen ? "openArrow" : ""}`}
+        />
+      </div>
+      <div className={`desciptionClps ${isOpen ? "open" : ""}`}>
+        <div className="textClps">{description}</div>
+      </div>
     </div>
   );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,24 @@
 import { Banner } from "../components/Banner";
+import { Collapse } from "../components/Collapse";
+import aboutData from "../data/aboutData.json";
 import bannerAbout from "../assets/Banner-about.png";
+
+import "../styles/about.scss";
 
 export function About() {
   return (
     <div>
       <Banner image={bannerAbout} />
+      <section>
+        {aboutData.map((dataClps) => (
+          <Collapse
+            key={dataClps.id}
+            id={dataClps.id}
+            title={dataClps.title}
+            description={dataClps.description}
+          />
+        ))}
+      </section>
     </div>
   );
 }
